11c16646c293bbc70bcec2969e245c29a935d3c8
ddqp <1361229831@qq.com>
2020-03-27 09:01:10 -0700

    EventLoopThread

--- ddserver/EventLoopThread.h
+++ ddserver/EventLoopThread.h
@@ -1,12 +1,22 @@
 #pragma once
+#include "Thread.h"
+#include "MutexLock_Util.h"
+#include "SyncCondition.h"
+#include "EventLoop.h"
 
 class EventLoopThread
 {
 public:
-    EventLoopThread() {}
-    ~EventLoopThread() {}
-
+    EventLoopThread();
+    ~EventLoopThread();
+    EventLoop * StartLoop();
 private:
-
+    bool exiting;
+    //BO style -> OO style
+    EventLoop * loop;
+    Thread thread_;
+    void threadfunc();
+    MutexLock mutex;
+    SyncCondition cond;
 };
 
--- ddserver/EventLoopThreadPooL.h
+++ ddserver/EventLoopThreadPooL.h
@@ -1,12 +1,23 @@
 #pragma once
-
+#include <vector>
+#include "EventLoop.h"
+#include  "EventLoopThread.h"
+#include "Logger.h"
 class EventLoopThreadPooL
 {
 public:
-    EventLoopThreadPooL() {}
-    ~EventLoopThreadPooL() {}
+    EventLoopThreadPooL(EventLoop * baseloop_,int numthreads_):baseloop(baseloop_),numthreads(numthreads_){}
+    ~EventLoopThreadPooL() {LOG<<"~EventLoopThreadPooL";}
 
+    EventLoop * GetNextLoop();
+    void Start();
 private:
+    bool started;
+    EventLoop * baseloop;
+    std::vector<EventLoopThread> threads;
+    std::vector<EventLoop> loops;
+    int numthreads;
+    int next_;
 
 };
 
